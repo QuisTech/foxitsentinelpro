cca7e802d4a6d42756386bcf1eae6ec7de9cf86a
QuisTech <michquis@gmail.com>
2026-02-18 08:16:52 +0100

    Debug: Use dynamic import to catch startup crashes

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,10 @@
-import app from "../server/app";
+// Remove static import
+// import app from "../server/app";
 
-export default function handler(req: any, res: any) {
+export default async function handler(req: any, res: any) {
   console.log(`[API] Incoming request: ${req.method} ${req.url}`);
   try {
+    const app = (await import("../server/app")).default;
     return app(req, res);
   } catch (err: any) {
     console.error("[CRITICAL] Request handler crashed:", err);
